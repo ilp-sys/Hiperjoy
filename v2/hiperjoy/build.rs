15ffb391ccc053b20a02fa146d1bb65511e8b16e
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-07-03 15:01:39 +0900

    resolve conflicts on README.md

--- v2/hiperjoy/build.rs
+++ v2/hiperjoy/build.rs
@@ -5,12 +5,17 @@ use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rustc-link-search=LeapSDK/lib");
+    println!("cargo:rustc-link-search=LeapSDK/samples");
     println!("cargo:rustc-link-lib=LeapC");
     println!("cargo:rustc-link-lib=LeapC.6");
+    println!("cargo:rustc-link-lib=exampleconnection");
     println!("cargo:rerun-if-changed=LeapSDK/include/LeapC.h");
+    println!("cargo:rerun-if-changed=LeapSDK/samples/ExampleConnection.h");
     
     let bindings = bindgen::Builder::default()
         .header("LeapSDK/include/LeapC.h")
+        .header("LeapSDK/samples/ExampleConnection.h")
+        .clang_arg(format!("-I{}", "LeapSDK/include"))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
--- v2/hiperjoy/src/main.rs
+++ v2/hiperjoy/src/main.rs
@@ -2,9 +2,10 @@ mod bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
+
 fn main() {
-    println!("hello world");
-//    unsafe {
-//        bindings::OpenConnection();
-//    }
+    println!("hello rust");
+    unsafe {
+        bindings::OpenConnection();
+    }
 }

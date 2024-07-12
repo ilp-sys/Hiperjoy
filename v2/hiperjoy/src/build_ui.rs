e3d5d3eff4a1e5157dbeb15af39065c557eb0773
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-07-12 10:44:31 +0900

    Feat: intialize gtk app

--- v2/hiperjoy/src/build_ui.rs
+++ v2/hiperjoy/src/build_ui.rs
@@ -1,11 +1,24 @@
-// use gtk::prelude::*;
-// use gtk::{Application, ApplicationWindow};
-//
-// pub fn build_ui(app: &Application) {
-//     let window = ApplicationWindow::builder()
-//         .application(app)
-//         .title("My GTK App")
-//         .build();
-//
-//     window.present();
-// }
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
+
+pub fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("activate")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button.connect_clicked(|button| {
+        button.set_label("Hello world");
+    });
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("HIPERJOY")
+        .child(&button)
+        .build();
+
+    window.present();
+}
--- v2/hiperjoy/src/main.rs
+++ v2/hiperjoy/src/main.rs
@@ -1,18 +1,18 @@
-// use gtk::prelude::*;
-// use gtk::{glib, Application};
-
-const APP_ID: &str = "hiperjoy";
-
-//use build_ui::build_ui;
-
-// fn main() -> glib::ExitCode {
-//     let app = Application::builder().application_id(APP_ID).build();
-//     app.connect_activate(build_ui);
-//     app.run()
-// }
-
+use gtk::prelude::*;
+use gtk::{glib, Application};
 use hiperjoy::leap_hand_sensor::leap_hand_sensor;
+use hiperjoy::build_ui::build_ui;
 
-fn main() {
-    leap_hand_sensor();
+const APP_ID: &str = "HIPERJOY";
+
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
 }
+
+
+// fn main() {
+//     leap_hand_sensor();
+// }

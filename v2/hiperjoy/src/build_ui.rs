b1f85095241dd1eaeb50fae374c3d148497c6327
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-07-17 16:50:25 +0900

    Feat: new line on log panel

--- v2/hiperjoy/src/build_ui.rs
+++ v2/hiperjoy/src/build_ui.rs
@@ -7,14 +7,6 @@ use gtk::{
 use std::sync::mpsc;
 use std::time::Duration;
 
-use crate::build_ui::glib::ControlFlow::Continue;
-
-fn append_log(log_panel: &TextView, message: &str) {
-    let buffer = log_panel.buffer();
-    buffer.insert(&mut buffer.end_iter(), message);
-    buffer.insert(&mut buffer.end_iter(), "\n");
-}
-
 pub fn build_ui(app: &Application) {
     let button = Button::builder()
         .label("deactivate")
@@ -37,7 +29,6 @@ pub fn build_ui(app: &Application) {
     vbox.append(&scrolled_window);
     vbox.append(&button);
 
-    //let (tx, rx) = mpsc::channel();
     let (tx, rx) = mpsc::channel();
     gio::spawn_blocking(move || {
         leap_hand_sensor(tx);
@@ -46,30 +37,15 @@ pub fn build_ui(app: &Application) {
     let log_panel_clone = log_panel.clone();
     glib::MainContext::default().spawn_local(async move {
         loop {
-            // Check for messages in a non-blocking way
             while let Ok(log_message) = rx.try_recv() {
-                // Clone the log_panel for use in the async block
                 let log_panel_clone_inner = log_panel_clone.clone();
-                // Execute the UI update in the main thread
                 glib::MainContext::default().spawn_local(async move {
-                    let buffer = log_panel_clone_inner
-                        .buffer();
+                    let buffer = log_panel_clone_inner.buffer();
                     buffer.insert_at_cursor(&log_message);
+                    buffer.insert_at_cursor("\n");
                 });
             }
-            // Sleep for a short duration to avoid busy-waiting
             glib::timeout_future(Duration::from_millis(100)).await;
-        }
-    });
-
-    button.connect_clicked(move |button| {
-        let current_label = button.label().expect("failed to get button label");
-        if current_label == "activate" {
-            button.set_label("deactivate");
-            append_log(&log_panel, "leap2 activate");
-        } else {
-            button.set_label("activate");
-            append_log(&log_panel, "leap2 activate");
         }
     });
 
@@ -77,6 +53,8 @@ pub fn build_ui(app: &Application) {
         .application(app)
         .title("HIPERJOY")
         .child(&vbox)
+        .default_width(600)
+        .default_height(400)
         .build();
 
     window.present();

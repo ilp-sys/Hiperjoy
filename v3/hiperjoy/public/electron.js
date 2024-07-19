7a4e2d073373818abf45d1f1421f1cb3d7bf334d
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-07-19 11:31:54 +0900

    fmt: prettier

--- v3/hiperjoy/public/electron.js
+++ v3/hiperjoy/public/electron.js
@@ -5,38 +5,38 @@ const isDev = await import("electron-is-dev");
 let mainWindow;
 
 function createWindow() {
-	mainWindow = new BrowserWindow({
-		width: 900,
-		height: 680,
-		webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true,
-			devTools: isDev,
-		},
-	});
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      devTools: isDev,
+    },
+  });
 
-	mainWindow.loadURL(
-		isDev
-			? "http://localhost:3000"
-			: `file://${path.join(__dirname, "../build/index.html")}`
-	);
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 
-	if (isDev) mainWindow.webContents.openDevTools({ mode: "detach" });
+  if (isDev) mainWindow.webContents.openDevTools({ mode: "detach" });
 
-	mainWindow.setResizable(true);
-	mainWindow.on("closed", () => {
-		mainWindow = null;
-		app.quit();
-	});
-	mainWindow.focus();
+  mainWindow.setResizable(true);
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+    app.quit();
+  });
+  mainWindow.focus();
 }
 
 app.on("ready", createWindow);
 
 app.on("activate", () => {
-	if (mainWindow === null) createWindow();
+  if (mainWindow === null) createWindow();
 });
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
--- v3/hiperjoy/src/App.tsx
+++ v3/hiperjoy/src/App.tsx
@@ -1,9 +1,17 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
 function App() {
-	return (
-		<div>
-			hello world
-		</div>
-	);
+  return (
+    <Container>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;

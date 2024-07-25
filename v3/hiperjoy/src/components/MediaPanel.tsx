f341eac869d4797106b6856e563b1675c3ced456
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-07-25 13:58:17 +0900

    Wip: media display

--- v3/hiperjoy/src/components/MediaPanel.tsx
+++ v3/hiperjoy/src/components/MediaPanel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
 import { styled } from "@mui/system";
+import RefreshRoundedIcon from "@mui/icons-material/RefreshRounded";
 import { useState, useEffect } from "react";
 import { parseStringPromise } from "xml2js";
 
@@ -14,6 +15,7 @@ import { buildXml } from "../utils/buildXml";
 import { fetchWrapper } from "../utils/fetchers";
 
 const MediaContainer = styled(Box)({
+  position: "relative",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
@@ -26,38 +28,61 @@ const MediaContainer = styled(Box)({
   boxShadow: "0 4px 10px rgba(0, 0, 0, 0.5)",
 });
 
+const RefreshButton = styled(IconButton)({
+  position: "absolute",
+  top: "10px",
+  right: "10px",
+});
+
 const MediaPanel: React.FC = () => {
   const [media, setMedia] = useState<ContentObject[]>([]);
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const contentsDefaultPath = "C:\\Users\\Public\\HiperWall\\contents\\";
 
   const xmlPayload = buildXml("Commands", {
     action: {
       "@type": "list",
-      filter: "type=Streamer;notopen",
+      filter: "open",
     },
   });
+
+  const handleRefresh = () => {
+    setRefreshKey((prevKey) => prevKey + 1);
+  };
+
   useEffect(() => {
     fetchWrapper(xmlPayload)
       .then((response) => parseStringPromise(response))
-      .then((parsedData) => setMedia(parsedData.Objects.Object))
-      .catch((error) => console.log("failed to parse xml"));
-  }, []);
+      .then((parsedData) => {
+        setMedia(parsedData.Objects.Object);
+      })
+      .catch((error) => console.log("failed to parse xml", error));
+  }, [refreshKey]);
 
   return (
     <MediaContainer>
+      <RefreshButton aria-label="refresh" onClick={handleRefresh}>
+        <RefreshRoundedIcon />
+      </RefreshButton>
       {media.length === 0 ? (
         <Typography>선택된 미디어가 없습니다.</Typography>
       ) : (
         media.map((item, index) => (
           <Box key={index} mb={2}>
-            {item.type === "image" && (
+            {item.type == "Image" && (
               <img
-                src={item.name}
+                src={"file:\\" + `${contentsDefaultPath}` + `${item.name}`}
                 alt={`media-${index}`}
                 style={{ width: "100%" }}
               />
             )}
-            {item.type === "video" && (
-              <video src={item.name} controls style={{ width: "100%" }} />
+            {item.type == "Movie" && (
+              <video
+                src={"file:\\" + `${contentsDefaultPath}` + `${item.name}`}
+                controls
+                style={{ width: "100%" }}
+              />
             )}
           </Box>
         ))

61a8c62ad04e7932b612efebbed83df1817bcdb4
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-07-26 14:23:25 +0900

    Feat: hover state

--- v3/hiperjoy/src/components/LabelBottomNavagation.tsx
+++ v3/hiperjoy/src/components/LabelBottomNavagation.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import MonitorRoundedIcon from "@mui/icons-material/MonitorRounded";
+import { Paper, BottomNavigation, BottomNavigationAction } from "@mui/material";
 import FolderIcon from "@mui/icons-material/Folder";
+import MonitorRoundedIcon from "@mui/icons-material/MonitorRounded";
 import VideocamRoundedIcon from "@mui/icons-material/VideocamRounded";
 import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
 
@@ -13,28 +12,44 @@ export default function LabelBottomNavigation() {
     setValue(newValue);
   };
 
+  const hoverStyles = {
+    "&:hover": {
+      color: "primary.main",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+    },
+  };
+
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Feeds"
-        value="feeds"
-        icon={<MonitorRoundedIcon />}
-      />
-      <BottomNavigationAction
-        label="Wallinfo"
-        value="wallinfo"
-        icon={<InfoRoundedIcon />}
-      />
-      <BottomNavigationAction
-        label="Camera"
-        value="camera"
-        icon={<VideocamRoundedIcon />}
-      />
-      <BottomNavigationAction
-        label="Folder"
-        value="folder"
-        icon={<FolderIcon />}
-      />
-    </BottomNavigation>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation value={value} onChange={handleChange}>
+        <BottomNavigationAction
+          label="Feeds"
+          value="feeds"
+          icon={<MonitorRoundedIcon />}
+          sx={hoverStyles}
+        />
+        <BottomNavigationAction
+          label="Wallinfo"
+          value="wallinfo"
+          icon={<InfoRoundedIcon />}
+          sx={hoverStyles}
+        />
+        <BottomNavigationAction
+          label="Camera"
+          value="camera"
+          icon={<VideocamRoundedIcon />}
+          sx={hoverStyles}
+        />
+        <BottomNavigationAction
+          label="Folder"
+          value="folder"
+          icon={<FolderIcon />}
+          sx={hoverStyles}
+        />
+      </BottomNavigation>
+    </Paper>
   );
 }
